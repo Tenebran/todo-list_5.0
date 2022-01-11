af1e08a07d2922a83ea63341db20b68ebc26294a
Sergej Garkusha <Tenebrann@googlemail.com>
2022-01-11 21:17:58 +0100

    project todolist style

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import './App.css';
+import { TodoList } from './Todolist';
+
+export type TaskType = {
+  title: string;
+  isDone: boolean;
+  id: number;
+};
+
+//G-R-UD
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+  //BLL
+
+  const todoListTitle1: string = 'What to learn';
+  const totoListTitle2: string = 'What to buy';
+  const task_1: Array<TaskType> = [
+    { title: 'HTML&CSS', isDone: true, id: 1 },
+    { title: 'JS', isDone: true, id: 2 },
+    { title: 'React', isDone: true, id: 3 },
+  ];
+  const task_2: Array<TaskType> = [
+    { title: 'CHAMPAGNE', isDone: true, id: 4 },
+    { title: 'BEER', isDone: true, id: 5 },
+    { title: 'FISH', isDone: true, id: 6 },
+  ];
+  //UI
+
+  return (
+    <div className="App">
+      <TodoList title={todoListTitle1} task={task_1} />
+      <TodoList title={totoListTitle2} task={task_2} />
+    </div>
+  );
 }
 
 export default App;
